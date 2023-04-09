657147665a5d0ae390cc2f3895a4d97d3363a8bb
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-09 09:25:56 -0500

    Update

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -5,9 +5,9 @@ const stuffCtrl = require('../controllers/stuff');
 const multer = require("../middleware/multer-config");
 
 router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
